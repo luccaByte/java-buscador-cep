74956f41115981a746408bd4852b03020cca28cb
lucas <lucaston2323@gmail.com>
2024-10-30 22:42:50 -0300

    feat: testando a api

--- src/com/java/cep/Main.java
+++ src/com/java/cep/Main.java
@@ -1,7 +1,32 @@
 package com.java.cep;
 
+import com.google.gson.Gson;
+import com.java.cep.models.Cep;
+import com.java.cep.services.ViaCepClient;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("OMG HI");
+        Scanner sc = new Scanner(System.in);
+        ViaCepClient viaCep = new ViaCepClient();
+        Gson gson = new Gson();
+
+        System.out.println("Digite o cep: ");
+        String input = sc.nextLine();
+
+        try {
+            String json = viaCep.buscaCep(input);
+            Cep cep = gson.fromJson(json, Cep.class);
+
+            System.out.println("Resultado da Busca:");
+            System.out.println(cep);
+
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Erro ao buscar o CEP: " + e.getMessage());
+        }
+
+        sc.close();
     }
 }
